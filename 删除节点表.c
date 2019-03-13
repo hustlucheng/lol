862cc837c02cbd243cf579e7e1c3d379483992a9
hustlucheng <48341611+hustlucheng@users.noreply.github.com>
2019-03-13 15:24:30 +0800

    Add files via upload

---
+++ 删除节点表.c
@@ -0,0 +1,16 @@
+void Delete( int  i, List *PtrL ) {
+    int  j;
+
+    if( i < 1 || i > PtrL->Last+1 )
+    {
+        printf ("不存在第%d个元素", i );
+        return ;
+    }
+
+    for ( j = i; j <= PtrL->Last; j++ )
+        PtrL->Data[j-1] = PtrL->Data[j];
+ 
+    PtrL->Last--;
+ 
+    return;
+}
